a96811bc5fa0b8ae71ae3e0d2d0323738235c510
Do Quoc Thuong <thuongdq@gmail.com>
2021-09-10 17:52:02 +0700

    change appjs

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import ProductsContainer from './containers/ProductsContainer';
 import MessageContainer from './containers/MessageContainer';
 import CartContainer from './containers/CartContainer';
 import Footer from './components/Footer';
-//video 68
-<<<<<<< HEAD
-// demo for git
-// thủ lam nưa
-=======
-// demo for git check again
-//h he he
->>>>>>> 88f9caa8e4e05b3512b23e4fcab834289ad806ee
+
+//Using github with thuongdq
 class App extends Component {
     render() {
         return (
